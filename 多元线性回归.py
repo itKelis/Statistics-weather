9c614736eb8e627e01c6cec09843c15795b69e56
itKelis <szjkelis@outlook.com>
2022-05-29 13:42:06 +0000

    整理注释

--- 多元线性回归.py
+++ 多元线性回归.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+
 class multi():
     def __init__(self,file,sheet_name,start,size,select = None) -> None:
         #start 代表起始年份
@@ -17,74 +18,89 @@ class multi():
 
     def countL(self,x,y):
         #计算l
-        l = ((np.sum(x*y))-(1/(x.shape[0]-1))*np.sum(x)*np.sum(y))
-        # print(y)
-        return l
+        return ((np.sum(x*y))-(1/(x.shape[0]-1))*np.sum(x)*np.sum(y))
 
-    def Ls(self):
-        l = np.zeros((4,4))     #创建保存l的矩阵
-        ll = np.zeros((4,4))
+    def sloveRegression(self):
+        #计算L矩阵并解算出估计方程
+        #计算出b，b0，Ly，y估计
+        l = np.zeros((4,4))
+
         for i in range(4):
             for j in range(4):
-                # if j >= i :             #i和j都是选择x几
+                # if j >= i :             
+                # #i和j都是选择x几
                 l[i][j] = self.countL(self.datas[i],self.datas[j])
-                ll[i][j] = np.cov(self.datas[i],self.datas[j])[0][1]
-        ly = np.zeros(4)            #计算ly
-        print("l矩阵:\n{}".format(l))
+        
+        ly = np.zeros(4)
         for i in range(4):
             ly[i] = self.countL(self.datas[i], self.y)
-        print("ly是{}".format(ly))
-        self.b = np.linalg.solve(l, ly)      #解算
-        print("b是{}".format(self.b))
-        self.b0 = np.mean(self.y) - (np.mean(self.datas[0])*self.b[0]) - (np.mean(self.datas[1])*self.b[1]) - (np.mean(self.datas[2])*self.b[2]) - (np.mean(self.datas[3])*self.b[3])
-        print("b0是{}".format(self.b0))
-        self.func = lambda x1,x2,x3,x4,b,b0:eval("{}*x1+{}*x2+{}*x3+{}*x4+{}".format("b[0]","b[1]","b[2]","b[3]","b0"))
+        
+        self.l = l
+        self.ly = ly
+        self.b = np.linalg.solve(l, ly)     
+        self.b0 = np.mean(self.y) - (np.mean(self.datas[0])*self.b[0]) 
+        - (np.mean(self.datas[1])*self.b[1]) 
+        - (np.mean(self.datas[2])*self.b[2]) 
+        - (np.mean(self.datas[3])*self.b[3])
 
-    def countU(self):
+        self.ye = lambda x1,x2,x3,x4,b,b0:eval("{}*x1+{}*x2+{}*x3+{}*x4+{}".format("b[0]","b[1]","b[2]","b[3]","b0"))
+
+    def checkRegression(self):
+        #计算U,Q,Lyy,F,R
         Us = []
         for i in range(self.datas.shape[1]):
-            Us.append((self.func(self.datas[0][i],self.datas[1][i],self.datas[2][i],self.datas[3][i],self.b,self.b0) - np.mean(self.y))**2)
+            Us.append((self.ye(self.datas[0][i],self.datas[1][i],self.datas[2][i],self.datas[3][i],self.b,self.b0) - np.mean(self.y))**2)
         self.U = np.sum(Us)
-        print("U是{}".format(self.U))
+
         Qs = []
         for i in range(self.datas.shape[1]):
-            Qs.append((self.y[i] - self.func(self.datas[0][i],self.datas[1][i],self.datas[2][i],self.datas[3][i],self.b,self.b0))**2)
+            Qs.append((self.y[i] - self.ye(self.datas[0][i],self.datas[1][i],self.datas[2][i],self.datas[3][i],self.b,self.b0))**2)
+
         self.Q = np.sum(Qs)
-        print("Q是{}".format(self.Q))
         self.Lyy = self.U + self.Q
-    
-    def countF(self):
         self.F = self.U/self.Q/(self.datas.shape[1]-2)
-        print("F是{}".format(self.F))
+        self.R = self.U/self.Lyy
+    
+    def printResult(self):
+        print(f'''L矩阵是:\n{self.l}\n
+        F是：{self.F}
+        R是：{self.R}
+        Lyy是：{self.Lyy}
+        Q是：{self.Q}
+        U是：{self.U}
+        Ly是：{self.ly}
+        ''')
         pass
 
     def dataprocess(self,data,sheet_name,size,select, dimension = None):
-        a = []      #a用来保存处理前的数据
+        a = []      
+        #a用来保存处理前的数据
         
-        #读取每个表所需年份的所有数据
         if dimension == None:
+            #读取每个表所需年份的所有数据
+            #手动输入数据维度，一元则直接读取数据，多元则循环读取数据
             for i in range(len(sheet_name)):
                 datas = np.array(data[sheet_name[i]])[:size+1,1:13]     
                 a.append(datas)
         else:
             datas = np.array(data)[:size+1,1:13]     
             a.append(datas)
-        #一元
-        # datas = np.array(data)[:size+1,1:13]     
-        # a.append(datas)
 
         #对每个表按所需月份做平均
-        if select is not None:      #select是每个选择月份，如果是列表则每个表都提取相同年份，如果则每个表提取所指定的年份
+        if select is not None:      
+            #select是每个选择月份，如果是列表则每个表都提取相同年份，如果则每个表提取所指定的年份
             for i in range(len(sheet_name)):
-                # for i in [1]:
                     b = np.transpose(a[i])
                     month = None
+
                     if isinstance(select,list):
+                        #如果选择的月份以列表形式传入
+                        #则用列表推导式读取对应月份数据
+                        #以字典形式传入 
+                        #则用列表推导式读取对应月份的值
                         month = np.array([b[j] for j in select])
                     elif isinstance(select,dict):
                         month = np.array([b[j] for j in select[sheet_name[i]]])
-                    else:
-                        pass
                     a[i] = np.mean(np.transpose(month),axis=1)
                     
         if dimension == None:
@@ -97,6 +113,6 @@ months = {"Arctic Oscillation":[11,0,1],"North Atlantic Oscillation":[11,0,1],"N
 # months = [1,4,5]
 
 multis = multi("train2.xlsx",sheet_name,1979,39,select = months)
-multis.Ls()
-multis.countU()
-multis.countF()
+multis.sloveRegression()
+multis.checkRegression()
+multis.printResult()
